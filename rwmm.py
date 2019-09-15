4aa94f7a9216f0b4da9c392c4ef742e1afb36997
Elliott Fogg <elliott.fogg@gmail.com>
2019-09-15 22:31:31 +0100

    Renamed class files

--- rwmm.py
+++ rwmm.py
@@ -1,7 +1,7 @@
 # The main file that is run to start the program
 import tkinter as tk
-from load_mod_information import data_object
-from gui import gui
+from class_data_object import data_object
+from class_gui import gui
 
 if __name__ == '__main__':
     root = tk.Tk()
